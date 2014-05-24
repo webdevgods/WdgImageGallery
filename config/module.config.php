8d529ddfc9e79a47964678dcc1481eeb1c21783c
Jessie Green <jessie.ims@gmail.com>
2014-05-24 11:23:39 -0500

    Start gallery

--- config/module.config.php
+++ config/module.config.php
@@ -1,5 +1,12 @@
 <?php
 return array(
+    'asset_manager' => array(
+        'resolver_configs' => array(
+            'paths' => array(
+                __DIR__ . '/../public',
+            ),
+        ),
+    ),
     'view_manager' => array(
         'template_path_stack' => array(
             __DIR__ . '/../view',

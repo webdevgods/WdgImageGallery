8e0bd8af5d12bbdf663455868b0572f329059209
Jessie Green <jessie.ims@gmail.com>
2014-05-24 11:10:50 -0500

    Start gallery

--- config/module.config.php
+++ config/module.config.php
@@ -5,6 +5,20 @@ return array(
             __DIR__ . '/../view',
         ),
     ),
+    'doctrine' => array(
+        'driver' => array(
+            'WdgImageGallery_driver' => array(
+                'class' => 'Doctrine\ORM\Mapping\Driver\AnnotationDriver',
+                'cache' => 'array',
+                'paths' => array(__DIR__ . '/../src/WdgImageGallery/Entity')
+            ),
+            'orm_default' => array(
+                'drivers' => array(
+                    'WdgImageGallery\Entity' => 'WdgImageGallery_driver'
+                )
+            )
+        )
+    ),
     'module_layouts' => array(
         'WdgImageGallery' => 'application/layout/layout',
     ),
